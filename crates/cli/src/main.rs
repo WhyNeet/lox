17c5b87e24e409505d6056d03943d010f8dc0196
WhyNeet <arc.software.x@gmail.com>
2024-12-26 22:13:04 +0300

    feat(cli): implement basic cli

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
+use lexer::scanner::Scanner;
+
 fn main() {
-    println!("Hello, world!");
+    let input = env::args().nth(1).unwrap();
+
+    let scanner = Scanner::new(input);
+    scanner.scan_tokens();
+
+    println!("tokens: {:?}", scanner.tokens());
 }
