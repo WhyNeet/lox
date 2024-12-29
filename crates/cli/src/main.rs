1ed3c8fe7d730023f309450eb2b905f305f16524
WhyNeet <arc.software.x@gmail.com>
2024-12-29 13:27:30 +0300

    feat(cli): add parsing

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,15 +1,26 @@
 use std::{env, process};
 
 use lexer::scanner::Scanner;
+use parser::Parser;
 
 fn main() {
     let input = env::args().nth(1).unwrap();
 
     let scanner = Scanner::new(input);
     scanner.scan_tokens().unwrap_or_else(|err| {
-        println!("{err}");
+        eprintln!("{err}");
         process::exit(1)
     });
 
-    println!("tokens: {:?}", scanner.tokens());
+    let tokens = scanner.tokens();
+
+    println!("tokens: {:?}", tokens);
+
+    let parser = Parser::new(tokens);
+    let tree = parser.run().unwrap_or_else(|err| {
+        eprintln!("{err}");
+        process::exit(1)
+    });
+
+    println!("  -> {tree:?}");
 }
